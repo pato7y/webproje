a54c8472bdd4ed24aaca40f002e6e5855e39ba9e
pato7y <serhatyildiz721@gmail.com>
2024-03-11 18:38:01 +0300

    changes done

--- OneDrive/Masaüstü/web/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ OneDrive/Masaüstü/web/WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+80d18a6e8f96fcc050dc1d211423f583280df636")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1d76e83a910743633ef1d34a895437b51d2005c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
