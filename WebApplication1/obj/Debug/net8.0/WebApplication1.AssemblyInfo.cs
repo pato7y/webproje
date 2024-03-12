6fab30d2e545d8eb539d59daf618f3d31ca99760
pato7y <serhatyildiz721@gmail.com>
2024-03-12 23:50:38 +0300

    tried

--- WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net8.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e179260245cf3dbc16f9667adb5d32ae947c5d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e1bf96ab090903dbf467edcae1a6f3eff11ea18")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
